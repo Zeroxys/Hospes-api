b067b0125d78db751e6074cdb3af7d501b056867
Miguel Zavala <miguelzavalac@gmail.com>
2017-03-16 11:53:58 -0600

    Test para hacer GET de un usuario

--- test/stub/db.js
+++ test/stub/db.js
@@ -33,4 +33,8 @@ export default class Db {
   saveUser (user) {
     return Promise.resolve(fixtures.getUser())
   }
+
+  getUser (username) {
+    return Promise.resolve(fixtures.getUser())
+  }
 }
